64e73fc7d1a93673eeea64e5d9fa20b9dacbb73f
Matheus Costa <matheuscostadosantos@hotmail.com>
2021-05-01 16:59:48 -0300

    Aula 05

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -6,7 +6,10 @@ import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/ConvertDurationToTimeString';
 import styles from './episode.module.scss';
 import Image from 'next/image';
+import Head from 'next/head';
 import Link from 'next/link';
+import React, { useContext } from 'react';
+import { PlayerContext, usePlayer } from '../../contexts/PlayerContext';
 
 type Episode = {
   id: string;
@@ -25,8 +28,15 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
+  const { play } = usePlayer();
+
   return (
     <div className={styles.episode}>
+
+    <Head>
+      <title>{episode.title} | Podcasrtr</title>
+    </Head>
+
       <div className={styles.thumbnailContainer}>
         <Link href="/">
         <button type="button">
@@ -39,7 +49,7 @@ export default function Episode({ episode }: EpisodeProps) {
           src={episode.thumbnail}
           objectFit="cover"
         />
-        <button type="button">
+        <button type="button" onClick={() => play(episode)}>
           <img src="/play.svg" alt="Tocar episódio"/>
         </button>
       </div>
